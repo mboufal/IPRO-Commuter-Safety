ab81a36bb8038352a4a3685ea058f775155f9dad
hannahleland <hannah@hleland.com>
2022-02-22 19:55:02 -0500

    add DBConnect. reorganize images

---
+++ Code/js/DBConnect.js
@@ -0,0 +1,15 @@
+/*
+IPRO 497 - Team E - Spring 2022
+
+Authors: 
+- Hannah Leland
+
+Description: 
+This class 
+
+*/
+
+// Initialize Parse
+Parse.initialize("yApdhvS2ZXITMSEbQHiA5POfzdk86Zzyebgf8rO7", "dqZnClcyIPMBHMYmj7llcN1xPsEz8GvX0Qx2fhFG"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.serverURL = "https://parseapi.back4app.com/";
+
