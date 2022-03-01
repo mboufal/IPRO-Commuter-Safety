584e9499d5b660410ea7fa7e0917e16cec32e022
hannahleland <hannah@hleland.com>
2022-03-01 04:14:47 -0500

    report-form.js working. report.js not working

--- Code/js/DBConnect.js
+++ Code/js/DBConnect.js
@@ -1,15 +1,10 @@
 /*
 IPRO 497 - Team E - Spring 2022
 
-Authors: 
-- Hannah Leland
-
 Description: 
-This class 
-
+This file connects to the Back4App database using the Parse library.
 */
 
 // Initialize Parse
 Parse.initialize("yApdhvS2ZXITMSEbQHiA5POfzdk86Zzyebgf8rO7", "dqZnClcyIPMBHMYmj7llcN1xPsEz8GvX0Qx2fhFG"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 Parse.serverURL = "https://parseapi.back4app.com/";
-
