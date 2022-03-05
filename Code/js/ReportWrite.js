1cb60fbcd85e34497e40fcb337bf79c29a4c9da0
hannahleland <hannah@hleland.com>
2022-03-04 21:09:39 -0500

    user geolocation added to form. successfully submits to DB

--- Code/js/ReportWrite.js
+++ Code/js/ReportWrite.js
@@ -5,6 +5,25 @@ Description:
 This file takes data from report-form.html and creates a report in the database.
 */
 
+// Prompt user to permit geolocation coordinates via their browser
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(populatePosition);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Populate the form and the model with the coordinate data
+function populatePosition(position) {
+  let lat = parseFloat(position.coords.latitude); 
+  let long = parseFloat(position.coords.longitude); 
+
+  document.getElementById("lat").value = lat;
+  document.getElementById("long").value = long;
+}
+
 // Create a new Report
 async function createParseReport() {
   // Create new Parse object: "Report"
@@ -12,8 +31,9 @@ async function createParseReport() {
   console.log("report object created");
   let user_id = parseInt(document.getElementById("user-id").value);
   let report_id = parseInt(document.getElementById("report-id").value);
-  let lat = parseFloat(document.getElementById("lat").value);
+  let lat = parseFLoat(document.getElementById("lat").value);
   let long = parseFloat(document.getElementById("long").value);
+
 
   // Set the input values to the new "Report" object
   report.set("user_id", user_id);
@@ -40,6 +60,11 @@ async function createParseReport() {
     alert(`Error: ${error.message}`);
   }
 }
+
+// on click listener for location permission prompt
+document.getElementById("locate").addEventListener("click", async function () {
+  getLocation();
+});
 
 // Add on click listener to call the create parse report function
 document.getElementById("submit").addEventListener("click", async function () {
