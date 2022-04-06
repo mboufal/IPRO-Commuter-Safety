d15aca18dbfb5b9851565cf573d1ac75c35c5c14
hannahleland <hannah@hleland.com>
2022-04-05 22:08:00 -0400

    added incident type to the reports page list items. simplified report data presentation

--- Code/js/ReportRead.js
+++ Code/js/ReportRead.js
@@ -8,6 +8,7 @@ This file takes data from report table of the database and displays it to the in
 // Read reports
 (async () => {
   const Reports = Parse.Object.extend('Reports');
+  const Accidents = Parse.Object.extend('Accidents');
   const query = new Parse.Query(Reports);
   var incident_display = document.getElementById("incidents");
   // You can also query by using a parameter of an object
@@ -19,12 +20,30 @@ This file takes data from report table of the database and displays it to the in
       const report_date = object.get('report_date').toLocaleString();
       const accident_location = object.get('accident_location');
       const user_id = object.get('user_id');
+      const accident_id = object.get('accident_id');
+      let accident_title = "";
       console.log(report_date);
       console.log(accident_location);
       console.log(user_id);
 
+      if (accident_id == "A1") {
+        accident_title = "Robbery";
+      }
+      else if (accident_id == "A2") {
+        accident_title = "Assault";
+      } 
+      else if (accident_id == "A3") {
+        accident_title = "Verbal Harassment";
+      } 
+      else if (accident_id == "A4") {
+        accident_title = "Flu Outbreak";
+      } 
+      else if (accident_id == "A5") {
+        accident_title = "Paintball Shooting";
+      }
+
       var div = document.createElement("div");
-      div.innerHTML = 'Location: (' + accident_location.latitude + ', '+ accident_location.longitude + ') - User ID: ' + user_id + ' - Report Date: ' + report_date;
+      div.innerHTML = accident_title + ' - (' + accident_location.latitude + ', '+ accident_location.longitude + ') - ' + report_date;
       incident_display.appendChild(div);
     }
   } catch (error) {
